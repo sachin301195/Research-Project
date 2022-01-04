f297f93591b3844c91bd795556bb64ab8451b9a9
Bulchandani, Sachin <sachin.bulchandani@iosb-ina.fraunhofer.de>
2022-01-04 21:18:11 +0100

    corrected the eorror for the np array

--- cmfs_main.py
+++ cmfs_main.py
@@ -134,7 +134,7 @@ if __name__ == '__main__':
             "mask": True
         },
         "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
-        "num_workers": 1,  # parallelism
+        "num_workers": 15,  # parallelism
         "framework": args.framework
         },
         **cfg)
