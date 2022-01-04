c9a13c400bdcca7e311057607ecc14403c397a90
Bulchandani, Sachin <sachin.bulchandani@iosb-ina.fraunhofer.de>
2022-01-04 18:35:15 +0100

    corrected the eorror for the np array

--- conveyor_environment/conveyor_environment/envs/conveyor_network_v3.py
+++ conveyor_environment/conveyor_environment/envs/conveyor_network_v3.py
@@ -184,7 +184,7 @@ class ConveyorEnv_v3(gym.Env):
 
         self.reward_range = [-1, 1, self.final_reward]
         # Observation space represents the places
-        obs_space = spaces.Box(0, 1, shape=(self.no_places,))
+        obs_space = spaces.Box(0, 1, shape=(self.no_places + 3,))
         # Action space represents the transitions to get fired
         self.action_space = spaces.Discrete(5)
         if self.mask:
