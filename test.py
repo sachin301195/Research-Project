3a4933d2c53e3e7ca4abee03cb691e347398ea18
Bulchandani, Sachin <sachin.bulchandani@iosb-ina.fraunhofer.de>
2022-01-09 14:12:27 +0100

    trial for get_pass

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import getpass
+from pathlib import Path
+import platform
 
 if getpass.getuser() == 'sachin':
     print('I got you Sachin')
 else:
     print('Try something else')
+print(platform.system())
