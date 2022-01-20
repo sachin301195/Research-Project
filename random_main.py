2cb4eaf163d3592f81437948bc97df8d482de523
Bulchandani, Sachin <sachin.bulchandani@iosb-ina.fraunhofer.de>
2022-01-20 03:38:35 +0100

    no_of_epsode to 200

--- random_main.py
+++ random_main.py
@@ -36,7 +36,7 @@ REWARD_RESULTS_PATH = '/reward-results/'
 AVG_OVR_EP_PATH = '/avg_over_ep-results/'
 CHECKPOINT_ROOT = './checkpoints'
 
-NUM_EPISODES = 1000
+NUM_EPISODES = 200
 REWARDS = []
 AVG_THROUGHPUT = []
 ORDER_THROUGHPUT = []
