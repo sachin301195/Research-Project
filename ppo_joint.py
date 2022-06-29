06e36d214df4847d3f6996605ce0c5cbb9fa460f
Bulchandani Sachin <bulchandani.sachin@iosb-ina.fraunhofer.de>
2022-06-29 14:28:16 +0200

    joint training FOR 10 JOBS random search for entropy coefficient

--- ppo_joint.py
+++ ppo_joint.py
@@ -246,7 +246,9 @@ if __name__ == '__main__':
             "vf_clip_param": 10,
             # "lr": tune.grid_search([0.001, 0.0001])
             "lr": 0.0001,
-            "entropy_coeff": tune.loguniform(0.0001, 0.001)
+            "entropy_coeff": tune.grid_search([tune.uniform(0.0001, 0.001), tune.uniform(0.0001, 0.001),
+                                               tune.uniform(0.0001, 0.001), tune.uniform(0.0001, 0.001),
+                                               tune.uniform(0.0001, 0.001)])
             # "horizon": 32,
             # "timesteps_per_batch": 2048,
         },
