5c5b8954f159a82a695e103c41d7c8c6db7ad3c2
Bulchandani Sachin <bulchandani.sachin@iosb-ina.fraunhofer.de>
2022-07-02 12:19:47 +0200

    lr changed to 0.0001

--- ppo_main_evaluation.py
+++ ppo_main_evaluation.py
@@ -364,7 +364,7 @@ if __name__ == '__main__':
             "model": {
                 "custom_model": f"env_cfms_{args.env[-1]}",
                 "vf_share_layers": True,
-                "fcnet_hiddens": [256, 256, 256]
+                # "fcnet_hiddens": [256, 256, 256]
             },
             "env_config": {
                 "version": "full",
