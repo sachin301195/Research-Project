4092670f2fa80f8516e0e7c8570548519ef97efb
Bulchandani Sachin <bulchandani.sachin@iosb-ina.fraunhofer.de>
2022-07-12 15:20:50 +0200

    Stopping ALGO

--- ppo_joint.py
+++ ppo_joint.py
@@ -319,7 +319,7 @@ if __name__ == '__main__':
 
     stop = {
         # "training_iteration": 100 * args.no_of_jobs,
-        "episode_reward_mean": 29.6,
+        "episode_reward_mean": 30 - (35 * args.no_of_jobs * 0.002),
     }
     plots_save_path, agent_save_path, best_agent_save_path = setup(args.algo, args.no_of_jobs, args.env, timestamp)
 
