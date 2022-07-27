cf9e6f6aa77b6e6a7bb1d67c12f0ec2f2b2ea44b
Bulchandani Sachin <bulchandani.sachin@iosb-ina.fraunhofer.de>
2022-07-27 10:55:54 +0200

    removed unnecessary print statements added stop

--- ppo_joint.py
+++ ppo_joint.py
@@ -463,7 +463,7 @@ if __name__ == '__main__':
     # , resources_per_trial = ppo.PPOTrainer.default_resource_request(algo_config)
     result = tune.run(args.algo, config=algo_config, local_dir=best_agent_save_path, log_to_file=True,
                       checkpoint_at_end=True, checkpoint_freq=50, reuse_actors=False, verbose=3,
-                      checkpoint_score_attr='min-episode_len_mean')
+                      checkpoint_score_attr='min-episode_len_mean', stop=stop)
     logger.info(result)
     print('...............................................................................\n'
           '\n\n\t\t\t\t\t\t\t\t Training Ends Here\n\n\n........................................')
